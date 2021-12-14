074ba6d1e1acf750c07275dcd29d754f4eb6c482
Thales <47369312+tminet@users.noreply.github.com>
2021-12-14 11:30:41 -0300

    added repository bind with paging source implementation

--- app/src/main/java/tmidev/marvelheroes/framework/di/DataModule.kt
+++ app/src/main/java/tmidev/marvelheroes/framework/di/DataModule.kt
@@ -1,10 +1,14 @@
 package tmidev.marvelheroes.framework.di
 
+import androidx.paging.PagingSource
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import tmidev.core.data.datasource.RemoteCharactersDataSource
+import tmidev.core.data.repository.CharactersRepository
+import tmidev.core.domain.model.Character
+import tmidev.marvelheroes.framework.CharactersRepositoryImpl
 import tmidev.marvelheroes.framework.remote.response.DataWrapperResponse
 import tmidev.marvelheroes.framework.remote.retrofit.RetrofitCharactersDataSource
 
@@ -15,4 +19,9 @@ interface DataModule {
     fun bindRemoteCharactersDataSource(
         datasource: RetrofitCharactersDataSource
     ): RemoteCharactersDataSource<DataWrapperResponse>
+
+    @Binds
+    fun bindCharactersRepository(
+        repository: CharactersRepositoryImpl
+    ): CharactersRepository<PagingSource<Int, Character>>
 }
